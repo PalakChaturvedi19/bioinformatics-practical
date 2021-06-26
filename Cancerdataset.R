d1c30ea2272fbf6685bc05df874b3aee21d26220
PalakChaturvedi19 <85552477+PalakChaturvedi19@users.noreply.github.com>
2021-06-26 10:41:18 +0530

    Update Cancerdataset.R

--- Cancerdataset.R
+++ Cancerdataset.R
@@ -1,6 +1,12 @@
 library(ggplot2)
 ?ggplot2
 
+library(corrplot)
+
+library(caTools)
+
+library(dplyr)
+
 
 cancer = read.csv(choose.files(),header = TRUE)
 cancer
@@ -53,10 +59,12 @@ ggplot(cancer,aes(x=diagnosis,fill=texture_mean))+
 
 #density plot based on texture and mean
 ggplot(cancer,aes(x = texture_mean,fill = as.factor(diagnosis)))+geom_density()+ggtitle("texture_mean")
+
 # barplot for area_se
 ggplot(cancer,aes(x=diagnosis,fill=area_se))+
   geom_bar(fill = "purple")+
   ggtitle("AREA_SE")
+
 #distrubution of data by histogram
 ggplot(cancer,aes(x = concavity_mean,fill = diagnosis)
 )+geom_histogram(binwidth=5)+ggtitle("concavity_mean")
@@ -64,6 +72,7 @@ ggplot(cancer,aes(x=perimeter_mean))+facet_wrap(~diagnosis)+
   geom_histogram(binwidth=15,col="green",fill="pink")+ggtitle("concavity_mean")
 ggplot(cancer,aes(x=radius_mean))+facet_wrap(~diagnosis)+
   geom_histogram(binwidth=1,col="green",fill="pink")+ggtitle("radius_mean")
+
 # creating pdf
 pdf("cancer,plots2.pdf")
 pietables = sprintf("%s-%3.1f%s",diagnosis.prop.df[,1],
